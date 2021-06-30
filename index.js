746a695c7bff9d6b07e9c8c83fa0cc390ae1ac00
Samoel33 <hsseshoka@gmail.com>
2021-06-30 14:13:51 +0200

    all changes

---
+++ index.js
@@ -0,0 +1,35 @@
+const body = document.querySelector('body');
+
+function showTime() {
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    var session = "AM";
+
+
+    if (hours > 12) {
+        hours = hours - 12;
+        session = "PM";
+        body
+    }
+
+    if (hours == 0) {
+        hours = 12;
+    }
+
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+    var time = hours + ":" + minutes + ":" + seconds + " " + session;
+
+
+    document.getElementById("clock").innerText = time;
+    document.getElementById("clock").textContent = time;
+
+    setTimeout(showTime, 1000);
+
+}
+
+showTime();
